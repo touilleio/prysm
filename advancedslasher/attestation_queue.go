15a0c6626a4bf836868b044d29b9514f8ae1a12c
Raul Jordan <raul@prysmaticlabs.com>
2020-12-26 22:02:53 -0600

    more att queue improvements

--- advancedslasher/attestation_queue.go
+++ advancedslasher/attestation_queue.go
@@ -14,12 +14,6 @@ func (q *attestationQueue) push(att *ethpb.IndexedAttestation) {
 	q.queue = append(q.queue, att)
 }
 
-func (q *attestationQueue) pop() *ethpb.IndexedAttestation {
-	el := q.queue[0]
-	q.queue = q.queue[1:]
-	return el
-}
-
 func (q *attestationQueue) dequeue() []*ethpb.IndexedAttestation {
 	elems := q.queue
 	q.queue = make([]*ethpb.IndexedAttestation, 0)
