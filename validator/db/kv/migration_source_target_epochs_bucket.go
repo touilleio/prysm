8462ccd84d7e389f6ca339f1bcf9bed9a0f94263
Raul Jordan <raul@prysmaticlabs.com>
2021-02-16 19:34:34 -0600

    begin new migration logic

--- validator/db/kv/migration_source_target_epochs_bucket.go
+++ validator/db/kv/migration_source_target_epochs_bucket.go
@@ -2,14 +2,50 @@ package kv
 
 import (
 	"bytes"
+	"context"
 	"errors"
 
-	"go.etcd.io/bbolt"
+	bolt "go.etcd.io/bbolt"
 )
 
 var migrationSourceTargetEpochsBucketKey = []byte("source_target_epochs_bucket_0")
 
-func migrateSourceTargetEpochsBucket(tx *bbolt.Tx) error {
+func (s *Store) migrateSourceTargetEpochsBucketUp(ctx context.Context) error {
+	// First, we extract the public keys we need to migrate data for.
+	publicKeyBytes := make([][]byte, 0)
+	err := s.db.View(func(tx *bolt.Tx) error {
+		mb := tx.Bucket(migrationsBucket)
+		if b := mb.Get(migrationSourceTargetEpochsBucketKey); bytes.Equal(b, migrationCompleted) {
+			return nil // Migration already completed.
+		}
+		bkt := tx.Bucket(pubKeysBucket)
+		return bkt.ForEach(func(k, _ []byte) error {
+			if k == nil {
+				return nil
+			}
+			nk := make([]byte, len(k))
+			copy(nk, k)
+			publicKeyBytes = append(publicKeyBytes, nk)
+			return nil
+		})
+	})
+	if err != nil {
+		return err
+	}
+
+	// Next up, we initiate a bolt transaction for each public key.
+	for _, pubKey := range publicKeyBytes {
+		err = s.db.Update(func(tx *bolt.Tx) error {
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func migrateSourceTargetEpochsBucket(tx *bolt.Tx) error {
 	mb := tx.Bucket(migrationsBucket)
 	if v := mb.Get(migrationSourceTargetEpochsBucketKey); bytes.Equal(v, migrationCompleted) {
 		return nil
@@ -49,7 +85,7 @@ func migrateSourceTargetEpochsBucket(tx *bbolt.Tx) error {
 	return mb.Put(migrationSourceTargetEpochsBucketKey, migrationCompleted)
 }
 
-func insertTargetSource(bkt *bbolt.Bucket, targetEpochBytes, sourceEpochBytes []byte) error {
+func insertTargetSource(bkt *bolt.Bucket, targetEpochBytes, sourceEpochBytes []byte) error {
 	var existingAttestedSourceBytes []byte
 	if existing := bkt.Get(targetEpochBytes); existing != nil {
 		existingAttestedSourceBytes = append(existing, sourceEpochBytes...)
